bb6ccccb2829fd425e03db8ec42dafb5e5007689
combatd <combatdstar@gmail.com>
2019-06-19 12:22:01 -0700

    exercises from app academy open

---
+++ software_engineering_fundamentals/rspec_exercise_1/lib/part_1.rb
@@ -0,0 +1,23 @@
+# takes average of two number arguments
+def average (numOne, numTwo)
+    sum = numOne + numTwo
+    avg = sum / 2.0
+    
+    return avg
+end
+
+# takes average of all numbers in array argument
+def average_array (array)
+    return array.sum * 1.0 / array.length * 1.0 
+end
+
+# accepts string and repeats it number of times
+def repeat (str, num)
+    newStr = ''
+    
+    num.times do
+        newStr << str
+    end
+
+    return newStr
+end
