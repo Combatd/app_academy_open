ee186cd96d1f4c03ba018c4206e7fe154150c006
combatd <combatdstar@gmail.com>
2019-06-20 14:53:29 -0700

    merge method for hash

--- software_engineering_fundamentals/rspec_exercise_2/lib/part_1.rb
+++ software_engineering_fundamentals/rspec_exercise_2/lib/part_1.rb
@@ -16,3 +16,23 @@ def partition (arr, num)
     partArr[1] = moreArr
     partArr # return partArr
 end
+
+def merge (hash_1, hash_2)
+    mergedHash = Hash.new(0)
+    
+    hash_1.each do |k,v|
+        if hash_2[k] === hash_1[k]
+            mergedHash[k] = hash_2[k] # take value of hash_2 if duplicate key
+        end      
+        mergedHash[k] = v
+    end
+
+    hash_2.each do |k,v|
+        if hash_2[k] === hash_1[k]
+            mergedHash[k] = hash_2[k]
+        end
+       mergedHash[k] = v 
+    end
+        
+    mergedHash # return the merged hash
+end
