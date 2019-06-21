dc4572677561905fe6013d784185e4d125b379c0
combatd <combatdstar@gmail.com>
2019-06-21 15:32:12 -0700

    solved substrings

--- software_engineering_fundamentals/rspec_exercise_2/lib/part_2.rb
+++ software_engineering_fundamentals/rspec_exercise_2/lib/part_2.rb
@@ -13,3 +13,22 @@ def palindrome? (str)
 
     return false
 end
+
+def substrings (string)
+    strArr = [] # empty array
+    i = 0
+
+    while i < string.length - 1
+        newStr = '' # string empties each loop iteration
+        
+        string.each_char.with_index do |char, j|
+            if j >= i
+                newStr += char
+                strArr << newStr # each time a char added, it is a new ele of strArr
+            end    
+        end # end each char loop
+        i += 1
+    end  # end while loop
+    strArr << string[-1] # shovel last character in the array
+    strArr # return the array
+end
