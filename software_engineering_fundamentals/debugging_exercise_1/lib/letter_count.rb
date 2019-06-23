e6f7e9618e5c2c8c26af54fd71a204b31727ecb2
combatd <combatdstar@gmail.com>
2019-06-23 12:15:03 -0700

    debugged letter_count.rb

--- software_engineering_fundamentals/debugging_exercise_1/lib/letter_count.rb
+++ software_engineering_fundamentals/debugging_exercise_1/lib/letter_count.rb
@@ -6,9 +6,14 @@
 require "byebug"
 
 def letter_count(string, char)
-    count = nil
-    string.each_char do |c| 
-        count += 1 if c = char 
+    count = 0 # initialize count with 0
+    # downcase for ease of comparisons
+    word = string.downcase
+    letter = char.downcase
+    word.each_char do |c| 
+        if c == letter # two equal signs for comparisons
+            count += 1 
+        end
     end
     count
 end
