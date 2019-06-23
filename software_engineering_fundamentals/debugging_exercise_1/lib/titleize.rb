b3c00bb908d64fee2b37869752aeee1f4a233604
combatd <combatdstar@gmail.com>
2019-06-23 11:40:19 -0700

    titleize method solved

--- software_engineering_fundamentals/debugging_exercise_1/lib/titleize.rb
+++ software_engineering_fundamentals/debugging_exercise_1/lib/titleize.rb
@@ -6,18 +6,22 @@
 
 require "byebug"
 
-little_words = [ "and", "the", "over", "a", "on", "of" ]
+
 
 def titleize(title)
   words = title.split(" ")
-
-  titleized_words = words.map.with_index do |i, word|
+  # local scope for little_words
+  little_words = [ "and", "the", "over", "a", "on", "of" ]
+  
+  # element , index for map method
+  titleized_words = words.map.with_index do |word, i| 
     if i == 0 || !little_words.include?(word)
       word.capitalize
+      # capitalize if not included in little_words array
     else
-      word.downcase
+      word # return the same word if not meeting conditions
     end
   end
-
+  
   titleized_words.join(" ")
 end
