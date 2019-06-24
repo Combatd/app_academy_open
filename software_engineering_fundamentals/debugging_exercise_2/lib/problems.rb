2fadb5fa60da0d807b0e2b3f1e1e1e3b0fe20e34
combatd <combatdstar@gmail.com>
2019-06-23 17:52:40 -0700

    largest_prime_factor solved

--- software_engineering_fundamentals/debugging_exercise_2/lib/problems.rb
+++ software_engineering_fundamentals/debugging_exercise_2/lib/problems.rb
@@ -1,3 +1,16 @@
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
+require "byebug"
+require "prime"
+
+def largest_prime_factor (num)
+   primeArr = [] # initialize empty array
+   Prime.each(num) do |prime|
+    primeArr << prime # shovel prime numbers into array
+   end
+   
+   primeArr.reverse.each do |ele|
+    return ele if num % ele == 0
+   end # return largest prime number in array
+end
