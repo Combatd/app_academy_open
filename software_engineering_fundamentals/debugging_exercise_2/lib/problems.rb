ff0c99374bb3fe3846d57fd5afe7adbc15330324
combatd <combatdstar@gmail.com>
2019-06-24 18:19:35 -0700

    unique_chars? solved

--- software_engineering_fundamentals/debugging_exercise_2/lib/problems.rb
+++ software_engineering_fundamentals/debugging_exercise_2/lib/problems.rb
@@ -14,3 +14,24 @@ def largest_prime_factor (num)
     return ele if num % ele == 0
    end # return largest prime number in array
 end
+
+def unique_chars?(str)
+   charsHash = Hash.new(0) # initialize new hash with 0 values by default
+    str.each_char.with_index do |k, i|
+      charsHash[k] = 0
+   end
+
+   
+   str.each_char.with_index do |char, j|
+      if charsHash.has_key?(char)
+            charsHash[char] += 1
+      end
+   end
+
+   charsHash.each_value do |value|
+      if value > 1
+         return false
+      end
+    end
+   return true
+end
