070cda48ac94e03bee0aea33799393c73fa4c78e
combatd <combatdstar@gmail.com>
2019-06-25 20:07:22 -0700

    solved reject_puppies
    used brace syntax for reject_puppies to target age property

--- software_engineering_fundamentals/blocks_project/lib/part_1.rb
+++ software_engineering_fundamentals/blocks_project/lib/part_1.rb
@@ -3,3 +3,7 @@ require "byebug"
 def select_even_nums (arr)
     return evenArr = arr.select(&:even?) 
 end
+
+def reject_puppies (dogs)
+    return dogsArr = dogs.reject{|dog| dog['age'] < 3 }
+end
