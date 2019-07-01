c6717971d022a4fb9e3eb765244e85a9bc37304a
combatd <combatdstar@gmail.com>
2019-06-30 21:40:06 -0700

    my_count solved

--- software_engineering_fundamentals/procs_project/lib/part_1.rb
+++ software_engineering_fundamentals/procs_project/lib/part_1.rb
@@ -11,3 +11,13 @@ def my_select (arr, &prc)
     arr.each { |num| newArr << num if prc.call(num) }
     newArr # return the new array
 end
+
+def my_count (arr, &prc)
+    count = 0 # initialize an accumulator
+
+    arr.each_with_index do |num|
+        count += 1 if prc.call(num)
+    end
+
+    count # return the number of elements that match prc conditions
+end
