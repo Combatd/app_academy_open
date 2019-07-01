fcaed049928e7b0df768cdd0ee119f50f8c068a0
combatd <combatdstar@gmail.com>
2019-06-30 21:35:37 -0700

    my_select solved

--- software_engineering_fundamentals/procs_project/lib/part_1.rb
+++ software_engineering_fundamentals/procs_project/lib/part_1.rb
@@ -5,3 +5,9 @@ def my_map (arr, &prc)
 
     newArr # return the new array
 end
+
+def my_select (arr, &prc)
+    newArr = []
+    arr.each { |num| newArr << num if prc.call(num) }
+    newArr # return the new array
+end
