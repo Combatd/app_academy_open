674b2324a58dd41b2ddee1ff411ca4576627899e
combatd <combatdstar@gmail.com>
2019-06-30 22:02:55 -0700

    my_any? solved

--- software_engineering_fundamentals/procs_project/lib/part_1.rb
+++ software_engineering_fundamentals/procs_project/lib/part_1.rb
@@ -15,9 +15,13 @@ end
 def my_count (arr, &prc)
     count = 0 # initialize an accumulator
 
-    arr.each_with_index do |num|
+    arr.each_with_index do |num, i|
         count += 1 if prc.call(num)
     end
 
     count # return the number of elements that match prc conditions
+end
+# ampersand converts proc to block of code
+def my_any? (arr, &prc)
+   return arr.each { |ele| prc.call(ele) }
 end
