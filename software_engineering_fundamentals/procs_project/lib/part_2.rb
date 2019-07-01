76c489f54391ce8311a5a1c0340c996b4285acb4
combatd <combatdstar@gmail.com>
2019-07-01 03:18:55 -0700

    word_changer solved
    used map for new array that runs the block on each word

--- software_engineering_fundamentals/procs_project/lib/part_2.rb
+++ software_engineering_fundamentals/procs_project/lib/part_2.rb
@@ -1,5 +1,14 @@
 def reverser (str, &prc) 
-    newStr = '' # reverse the string first
+    newStr = '' # empty string
     newStr += prc.call(str.reverse) # pass in str.reverse to block
     newStr # return the newStr
+end
+
+def word_changer (str, &prc)
+    newStr = '' # empty string
+    wordArr = str.split(' ') # split array into elements of each word
+    newArr = wordArr.map do |word, i| 
+        prc.call(word)
+    end
+    return newArr.join(' ') # join array back into one string
 end
