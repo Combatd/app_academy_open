95423a7f461f3d5e27654774ae7570224d503253
combatd <combatdstar@gmail.com>
2019-07-10 07:02:01 -0700

    #age= method

--- software_engineering_fundamentals/dog_project/lib/dog.rb
+++ software_engineering_fundamentals/dog_project/lib/dog.rb
@@ -1,3 +1,36 @@
 class Dog
+    # set method initializes entire Dog
+    def initialize (name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+    # get name
+    def name
+        @name # return instance variable
+    end
+    # get breed
+    def breed
+        @breed # return instance variable
+    end
+    # get age
+    def age
+       @age # return instance variable 
+    end
 
+    # set age
+    def age=(num) # user passes in new number
+        @age = num # sets instance variable age to a new number
+    end
+
+    # get bark
+    def bark
+        @bark # return instance variable
+    end
+    # get favorite_foods
+    def favorite_food?
+        @favorite_foods # return instance variable
+    end
 end
