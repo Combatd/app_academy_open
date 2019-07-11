f68fe8ce8e60e0c434233c6e1032d75b26ba6434
combatd <combatdstar@gmail.com>
2019-07-11 12:27:46 -0700

    bootcamp.rb part 1 complete

--- software_engineering_fundamentals/bootcamp_project/lib/bootcamp.rb
+++ software_engineering_fundamentals/bootcamp_project/lib/bootcamp.rb
@@ -11,6 +11,44 @@ class Bootcamp
     @grades = Hash.new{ |hash, key| hash[key] = [] }
     end
 
+    # get name instance
+    def name
+       @name 
+    end
 
+    # get slogan instance
+    def slogan
+        @slogan
+    end
+
+    # get teachers instance
+    def teachers
+        @teachers
+    end
+
+    # get students instance
+    def students
+        @students
+    end
+
+    # hire method takes string and shovels it into teachers array
+    def hire (teacher)
+        @teachers << teacher
+    end
+
+    # enroll will take a string and add them if below capacity
+    def enroll (student)
+        if @students.length < @student_capacity
+            @students << student # add the student to the array
+            true # the enrollment was successful
+        else
+            false # the enrollment was not successful
+        end
+    end
+
+    # enrolled? will return true or false based on student enrollment
+    def enrolled? (student)
+        @students.include?(student) # returns true or false on array existence
+    end
 
 end
