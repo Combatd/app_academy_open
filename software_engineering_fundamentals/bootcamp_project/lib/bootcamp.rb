f6c4b385263b6225508f0a4baaa69a42d11fe64b
combatd <combatdstar@gmail.com>
2019-07-11 12:16:26 -0700

    #initialize complete

--- software_engineering_fundamentals/bootcamp_project/lib/bootcamp.rb
+++ software_engineering_fundamentals/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,16 @@
 class Bootcamp
   
+    def initialize (name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    # instance variables teachers and students are empty arrays
+    @teachers = Array.new()
+    @students = Array.new()
+    # instance variable grades is a hash with empty array as default value
+    @grades = Hash.new{ |hash, key| hash[key] = [] }
+    end
+
+
+
 end
