1be16212da974e51e713eabea7411c10cd7ec05e
combatd <combatdstar@gmail.com>
2019-07-12 22:13:35 -0700

    span complete

--- software_engineering_fundamentals/monkey_patching_project/lib/array.rb
+++ software_engineering_fundamentals/monkey_patching_project/lib/array.rb
@@ -1,4 +1,15 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
   
+ def span
+    if self.length > 0
+        difference = self.max - self.min
+        return difference # return the difference
+    else
+        return nil      
+    end 
+ 
+ end
+
+ 
 end
