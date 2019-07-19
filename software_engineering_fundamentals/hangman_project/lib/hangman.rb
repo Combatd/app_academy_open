53d4b0cf8be6e0c1c3f88ec9e98cdf13b5e1fa5a
combatd <combatdstar@gmail.com>
2019-07-19 00:56:11 -0700

    initialize method done

--- software_engineering_fundamentals/hangman_project/lib/hangman.rb
+++ software_engineering_fundamentals/hangman_project/lib/hangman.rb
@@ -1,4 +1,15 @@
 class Hangman
-  DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+  DICTIONARY = ["cat", "dog", "bootcamp", "pizza"] # class constant
+  
+  def self.random_word # class method
+    DICTIONARY.sample
+  end
+  
+  def initialize # setter for instance variables
+    @secret_word = Hangman.random_word # set to random word from DICTIONARY
+    @guess_word = @secret_word.split("").map { |char| char = '_'  }
+    @attempted_chars = [] # initialize empty array
+    @remaining_incorrect_guesses = 5 # set guesses to 5, will be subtracted later
+  end
 
 end
