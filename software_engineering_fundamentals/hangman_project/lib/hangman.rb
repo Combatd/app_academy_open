2a3ecb48a3c41ca7b97ae9f6431118470143efe0
combatd <combatdstar@gmail.com>
2019-07-19 02:00:00 -0700

    part_1 of hangman_project complete

--- software_engineering_fundamentals/hangman_project/lib/hangman.rb
+++ software_engineering_fundamentals/hangman_project/lib/hangman.rb
@@ -12,4 +12,34 @@ class Hangman
     @remaining_incorrect_guesses = 5 # set guesses to 5, will be subtracted later
   end
 
+  def guess_word # get instance variable guess_word
+    @guess_word
+  end
+
+  def attempted_chars # get instance variable attempted_chars
+    @attempted_chars
+  end
+
+  def remaining_incorrect_guesses # get instance variable remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+
+  def already_attempted? (char) # boolean method checks if char was attempted
+    if @attempted_chars.include?(char)
+      return true
+    end
+
+    false # returns false if not included in @attempted_chars
+  end
+
+  def get_matching_indices (char)
+    matching_indices = [] # empty array
+    Hangman.random_word.each_char.with_index { |ele, i| matching_indices << i if ele == char }
+    matching_indices # return the array with all matching indices
+  end
+
+  def fill_indices (char, indices_arr)
+    indices_arr.each { |idx| @guess_word[idx] = char }
+  end
+
 end
