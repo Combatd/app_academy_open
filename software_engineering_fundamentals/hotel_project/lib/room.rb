443cef125f061b0a156aebcfed68400540a0957e
combatd <combatdstar@gmail.com>
2019-07-20 16:52:33 -0700

    room.rb complete

--- software_engineering_fundamentals/hotel_project/lib/room.rb
+++ software_engineering_fundamentals/hotel_project/lib/room.rb
@@ -1,3 +1,34 @@
 class Room
 
+ def initialize (capacity) # set instance variables
+    @capacity = capacity
+    @occupants = [] # empty array
+ end
+
+ # get methods for instance variables
+ def capacity 
+    @capacity
+ end
+
+ def occupants
+    @occupants
+ end
+
+ def full?
+    @occupants.length === @capacity # return true when capacity met
+ end
+
+ def available_space
+    @capacity - @occupants.length # returns the available space left
+ end
+
+ def add_occupant (name) # string name is arg
+    if self.full? 
+        return false
+    else        
+        @occupants << name # shovel name into array
+        return true
+    end
+ end
+
 end
