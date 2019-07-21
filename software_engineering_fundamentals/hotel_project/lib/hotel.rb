f3c22682c41c6102996fd0f1fb57e2231472164d
combatd <combatdstar@gmail.com>
2019-07-20 22:39:38 -0700

    Update hotel.rb

--- software_engineering_fundamentals/hotel_project/lib/hotel.rb
+++ software_engineering_fundamentals/hotel_project/lib/hotel.rb
@@ -1,5 +1,10 @@
 require_relative "room"
 
 class Hotel
-  
+    
+  def initialize (name, rooms)
+    @name = name
+    @rooms = Hash.new{ |room_name, room_cap| } # set rooms to new hash
+  end
+
 end
