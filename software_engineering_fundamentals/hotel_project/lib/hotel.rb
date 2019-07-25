49dd8e7b40c69529a6928d3dc21de4f3908c6988
combatd <combatdstar@gmail.com>
2019-07-25 02:16:30 -0700

    hotel_project completed

--- software_engineering_fundamentals/hotel_project/lib/hotel.rb
+++ software_engineering_fundamentals/hotel_project/lib/hotel.rb
@@ -1,10 +1,50 @@
 require_relative "room"
 
 class Hotel
-    
-  def initialize (name, rooms)
+  
+  def initialize (name, cap_hash)
     @name = name
-    @rooms = Hash.new{ |room_name, room_cap| } # set rooms to new hash
+    @rooms = {} # initialize empty hash
+    cap_hash.each do |room_name, room_cap|
+      @rooms[room_name] = Room.new(room_cap)
+    end
+
+  end
+
+  def name
+    capName = @name
+    capName.split(' ').each { |string| string.capitalize! }.join(' ')
+  end
+
+  def rooms
+    @rooms
+  end
+
+  def room_exists? (room)
+    @rooms.has_key?(room) # hash check
+  end
+
+  def check_in (person, room_name)
+    if self.room_exists?(room_name)
+      if @rooms[room_name].add_occupant(person)
+        puts "check in successful"  
+      else # if room is full
+        puts "sorry, room is full"
+      end # nested conditional
+    else
+      puts "sorry, room does not exist"   
+    end
+  
+  end
+
+  def has_vacancy?
+    @rooms.values.any? { |room| room.available_space > 0 } # check if room in hash has available space
+  end
+
+  def list_rooms
+    @rooms.each do |room_name, val|
+      puts "#{room_name} : #{val.available_space}"
+    end
   end
 
 end
