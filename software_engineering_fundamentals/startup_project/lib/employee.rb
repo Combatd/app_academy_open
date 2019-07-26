cb81eaf6107a7885046986a68db7b184e6fd514f
combatd <combatdstar@gmail.com>
2019-07-26 00:54:37 -0700

    employee.rb completed

--- software_engineering_fundamentals/startup_project/lib/employee.rb
+++ software_engineering_fundamentals/startup_project/lib/employee.rb
@@ -1,3 +1,16 @@
 class Employee
+    attr_reader :name, :title # getter method
+    
+  def initialize (name, title)
+    # set instance variables
+    @name = name
+    @title = title
+    # hidden encapuslated data
+    @earnings = 0 # accumulator
+  end
+
+  def pay (amount)
+    @earnings += amount # increase earnings by the amount
+  end
 
 end
