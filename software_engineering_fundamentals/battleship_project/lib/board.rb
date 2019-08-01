cfd8ad3a85d3875e5daff1619470d722e50e78f2
combatd <combatdstar@gmail.com>
2019-08-01 02:34:11 -0700

    battleship_project part 1 complete

--- software_engineering_fundamentals/battleship_project/lib/board.rb
+++ software_engineering_fundamentals/battleship_project/lib/board.rb
@@ -1,3 +1,32 @@
 class Board
+  attr_reader :size # getter methods  
   
+  def initialize (n)
+    board = Array.new(n) # take in the number
+    n.times do |row_idx|
+        board[row_idx] = Array.new(n) # 2D array
+        n.times do |col_idx|
+            board[row_idx][col_idx] = :N # each element will be N
+        end
+    end
+    @grid = board
+    @size = @grid.length * @grid.length # length times width
+  end
+
+  def [](pos) # syntactic sugar
+    row = pos[0]
+    col = pos[1]
+    @grid[row][col] # returns the element at this position
+  end
+
+  def []= (pos, val) # syntactic sugar
+    row = pos[0]
+    col = pos[1]
+    @grid[row][col] = val # set element at position to new value
+  end
+
+  def num_ships
+    @grid.count { |ele| ele.include?(:S)}  
+  end
+
 end
