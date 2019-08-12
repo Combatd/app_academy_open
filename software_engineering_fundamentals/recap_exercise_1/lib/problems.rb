253efc327ebc59e8cd8aeaf3cfb13933e9d7ea2d
combatd <combatdstar@gmail.com>
2019-08-11 19:59:19 -0700

    Update problems.rb

--- software_engineering_fundamentals/recap_exercise_1/lib/problems.rb
+++ software_engineering_fundamentals/recap_exercise_1/lib/problems.rb
@@ -61,7 +61,7 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
-        hash.new(prc)
+        
     end
 end
 
