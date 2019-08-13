81b9ff18736719fc1762aeaa7ecbeb829c8635ac
combatd <combatdstar@gmail.com>
2019-08-12 21:08:21 -0700

    #least_common_multiple solved

--- software_engineering_fundamentals/recap_exercise_2/lib/problems.rb
+++ software_engineering_fundamentals/recap_exercise_2/lib/problems.rb
@@ -1,7 +1,9 @@
 # Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple 
 # of both of the given numbers
 def least_common_multiple(num_1, num_2)
-
+    (1...100).each do |multiple|
+        return multiple if multiple % num_1 === 0 && multiple % num_2 === 0
+    end
 end
 
 
