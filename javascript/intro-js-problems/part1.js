41a0c2c003102c02bcb5b63f350864c90dca6fbb
Combatd <combatdstar@gmail.com>
2019-12-01 19:31:54 -0800

    create isPrime(num) in part1.js

--- javascript/intro-js-problems/part1.js
+++ javascript/intro-js-problems/part1.js
@@ -87,3 +87,26 @@ const fizzBuzz = (arr) => {
 }
 
 console.log(fizzBuzz([1,2,3,4,5,6,7,8,9,10]));
+
+function isPrime(num) {
+    if (num < 2) {
+        return false;
+    }
+
+    if (num === 2) {
+        return true;
+    }
+
+    for(let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPrime(2));
+console.log(isPrime(10));
+console.log(isPrime(15485863));
+console.log(isPrime(3548563));
