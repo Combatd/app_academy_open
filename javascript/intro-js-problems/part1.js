4f362ea8999f0c9f67825da3fcf6e5ebe65bb256
Combatd <combatdstar@gmail.com>
2019-12-01 19:06:39 -0800

    solve mystery scoping problems in part1.js

--- javascript/intro-js-problems/part1.js
+++ javascript/intro-js-problems/part1.js
@@ -1,29 +1,36 @@
-function mysteryScoping1() {
-    var x = 'out of block';
-    if (true) {
-        var x = 'in block';
-        console.log(x);
-    }
-    console.log(x);
-}
+// function mysteryScoping1() {
+//     var x = 'out of block';
+//     if (true) {
+//         var x = 'in block';
+//         console.log(x);
+//     }
+//     console.log(x);
+// }
 
-function mysteryScoping2() {
-    const x = 'out of block';
-    if (true) {
-        const x = 'in block';
-        console.log(x);
-    }
-    console.log(x);
-}
+// global scope var cannot be assigned twice!
 
-function mysteryScoping3() {
-    const x = 'out of block';
-    if (true) {
-        var x = 'in block';
-        console.log(x);
-    }
-    console.log(x);
-}
+// function mysteryScoping2() {
+//     const x = 'out of block';
+//     if (true) {
+//         const x = 'in block';
+//         console.log(x);
+//     }
+//     console.log(x);
+// }
+
+// mysteryScoping2();
+// will console log in block first because if statement runs first
+
+// function mysteryScoping3() {
+//     const x = 'out of block';
+//     if (true) {
+//         var x = 'in block';
+//         console.log(x);
+//     }
+//     console.log(x);
+// }
+
+// cannot declare var in local scope when a function level constant is declared
 
 function mysteryScoping4() {
     let x = 'out of block';
@@ -34,12 +41,18 @@ function mysteryScoping4() {
     console.log(x);
 }
 
-function mysteryScoping5() {
-    let x = 'out of block';
-    if (true) {
-        let x = 'in block';
-        console.log(x);
-    }
-    let x = 'out of block again';
-    console.log(x);
-}
+// mysteryScoping4();
+// declares a localized variable that can be confused for outer x
+
+// function mysteryScoping5() {
+//     let x = 'out of block';
+//     if (true) {
+//         let x = 'in block';
+//         console.log(x);
+//     }
+//     let x = 'out of block again';
+//     console.log(x);
+// }
+
+// cannot have two declarations of let x in same scope!
+//  should be variable reassignment instead
