d24745a9e2a43d81e5f21266b1e7f0eae98cb00e
Combatd <combatdstar@gmail.com>
2019-12-01 19:10:22 -0800

    create madLib(verb, adjective, noun) in part1.js

--- javascript/intro-js-problems/part1.js
+++ javascript/intro-js-problems/part1.js
@@ -56,3 +56,13 @@ function mysteryScoping4() {
 
 // cannot have two declarations of let x in same scope!
 //  should be variable reassignment instead
+
+// Write a function that takes three strings - a verb, an adjective, and a noun
+// uppercases and interpolates them into the sentence "We shall VERB the ADJECTIVE NOUN". 
+// Use ES6 template literals.
+
+const madLib = (verb, adjective, noun) => {
+    return `We shall ${verb.toUpperCase()} the ${adjective.toUpperCase()} ${noun.toUpperCase()}.`
+};
+
+console.log(madLib('make', 'best', 'guac'))
