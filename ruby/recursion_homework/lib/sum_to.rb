623953c59201da9c02f40ebad0dd3e30c4fce091
Combatd <combatdstar@gmail.com>
2019-12-21 15:00:35 -0800

    solve gamma_fnc(n) with quadratic complexity

--- ruby/recursion_homework/lib/sum_to.rb
+++ ruby/recursion_homework/lib/sum_to.rb
@@ -9,3 +9,11 @@ def add_numbers(nums_array)
     return nums_array.first if nums_array.length === 1
     nums_array.first + add_numbers(nums_array[1..-1])
 end
+
+def gamma_fnc(n)
+    return nil if !(n.is_a? (Numeric))
+    return nil if n < 1
+    return n if n === 1
+    originalNum = 0 + n
+    result = (n - 1) * gamma_fnc(n - 1)
+end
