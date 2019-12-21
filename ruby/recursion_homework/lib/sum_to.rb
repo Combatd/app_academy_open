bc7fa48ba37ff03e6627102afef0d986ecfa8783
Combatd <combatdstar@gmail.com>
2019-12-20 18:51:35 -0800

    solve add_numbers(nums_array) by removing the first element after the addition on each stack

--- ruby/recursion_homework/lib/sum_to.rb
+++ ruby/recursion_homework/lib/sum_to.rb
@@ -3,3 +3,9 @@ def sum_to(n)
     return n if n === 1
     n + sum_to(n - 1)
 end
+
+def add_numbers(nums_array)
+    return nil if nums_array.first === nil
+    return nums_array.first if nums_array.length === 1
+    nums_array.first + add_numbers(nums_array[1..-1])
+end
