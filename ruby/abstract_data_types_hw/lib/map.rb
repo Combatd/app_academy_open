1e6108eaf396a09b14c5f5069c8a1fdee77d7968
Combatd <combatdstar@gmail.com>
2019-12-24 00:32:51 -0800

    Set @map instance variable to empty array on class Map

---
+++ ruby/abstract_data_types_hw/lib/map.rb
@@ -0,0 +1,22 @@
+class Map
+    def initialize
+        @map = []
+    end
+
+    def set(key, value)
+    
+    end
+
+    def get(key)
+    
+    end
+
+    def delete(key)
+
+    end
+
+    def show
+
+    end
+
+end
