48620b6774eb7fc2fe94ab33a97f9a0c2d6ac329
Combatd <combatdstar@gmail.com>
2020-01-10 11:18:48 -0800

    Complete all tests in dessert_spec.rb

--- ruby/rspec_homework/spec/dessert_spec.rb
+++ ruby/rspec_homework/spec/dessert_spec.rb
@@ -68,6 +68,9 @@ describe Dessert do
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(brownie)
+      brownie.make_more
+    end
   end
 end
