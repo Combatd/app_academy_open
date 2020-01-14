187ed15e15785afa5cf88a5fcc2442c4747e9473
Combatd <combatdstar@gmail.com>
2020-01-14 12:06:33 -0800

    Create animals table

--- sql/create_db_postgres/my_tables.sql
+++ sql/create_db_postgres/my_tables.sql
@@ -3,3 +3,11 @@ CREATE TABLE countries(
     name VARCHAR(255) NOT NULL,
     population INTEGER NOT NULL
 );
+
+CREATE TABLE animals(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    country_id INTEGER NOT NULL,
+
+    FOREIGN KEY (country_id) REFERENCES countries(id)
+);
