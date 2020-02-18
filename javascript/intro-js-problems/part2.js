266486b80beec35d894a22df189cef4bdae5ef1d
Combatd <combatdstar@gmail.com>
2020-02-18 12:41:05 -0800

    Start intro-js-problems part 2

---
+++ javascript/intro-js-problems/part2.js
@@ -0,0 +1,6 @@
+// Write a function titleize that takes an array of names and a function (callback).
+// titleize should use Array.prototype.map to create a new array full of 
+// titleized versions of each name
+//  - titleize meaning "Roger" should be made to read "Mx. Roger Jingleheimer Schmidt".
+// Then pass this new array of names to the callback, 
+// which should use Array.prototype.forEach to print out each titleized name.
