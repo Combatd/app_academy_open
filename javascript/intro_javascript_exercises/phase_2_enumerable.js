ad21b0dade5cf1c4e0db2eae8cc1f42ae9ba9ce8
Combatd <combatdstar@gmail.com>
2020-02-20 15:27:07 -0800

    Refactor Array#myEach to run callback on each element of array

--- javascript/intro_javascript_exercises/phase_2_enumerable.js
+++ javascript/intro_javascript_exercises/phase_2_enumerable.js
@@ -4,6 +4,20 @@
 
 Array.prototype.myEach = function(callback) {
     for(let i = 0; i < self.length; i++) {
-        self[i].callback();
+        callback(this[i]);
     }
+}
+
+// Array#myMap(callback) - 
+// receives a callback function, returns a new array of the results of 
+// calling the callback function on each element of the array
+// should use myEach and a closure
+
+Array.prototype.myMap = function(callback) {
+    newArray = []
+    function runCallback() {
+        return self.myEach(callback);
+    } 
+    newArray = runCallback();
+    return newArray;
 }
