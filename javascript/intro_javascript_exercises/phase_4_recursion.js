c722789d025e953687ae3a6c9863ba1a1e878d47
Combatd <combatdstar@gmail.com>
2020-02-24 11:32:28 -0800

    Define sumRect(arr) on phase_4_recursion.js

--- javascript/intro_javascript_exercises/phase_4_recursion.js
+++ javascript/intro_javascript_exercises/phase_4_recursion.js
@@ -18,9 +18,24 @@ console.log(range(1, 5));
 
 // sumRec(arr) - receives an array of numbers and recursively sums them
 
+function sumRec(arr) {
+    if (!arr[0]) {
+        return false;
+    }
+    if (arr.length === 1) {
+        return arr[0];
+    }
+    
+    let index = arr.length - 1;
+    let sum = 0;
 
+    while(index >= 0) {
+        sum += arr[index];
+        index--;
+    }
+    return sum;
+}
 
-
-
+console.log(sumRec([1,2,3,4,5]), " sumRec");
 
 // exponent(base, exp) - receives a base and exponent, returns the base raise to the power of the exponent(base ^ exp)
