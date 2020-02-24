00756fffeeb65bd819ca359eb3f73498eba048c4
Combatd <combatdstar@gmail.com>
2020-02-24 10:48:22 -0800

    Define range(start,end) on phase_4_recursion.js

--- javascript/intro_javascript_exercises/phase_4_recursion.js
+++ javascript/intro_javascript_exercises/phase_4_recursion.js
@@ -1,7 +1,19 @@
 // range(start, end) - receives a start and end value, returns an array from start up to end
+const range = (start, end) => {
+    if (!start || !end) {
+        return self;
+    }
 
+    let index = 0 + end;
+    let rangeArr = [];
+    while (index >= start) {
+        rangeArr.unshift(index);
+        index--;
+    }
+    return rangeArr;
+}
 
-
+console.log(range(1, 5));
 
 
 // sumRec(arr) - receives an array of numbers and recursively sums them
