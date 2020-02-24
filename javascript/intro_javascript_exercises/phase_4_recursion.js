a94a717073211f2a6a75ff05794ba00f1f4e7480
Combatd <combatdstar@gmail.com>
2020-02-24 10:21:27 -0800

    Setup phase_4_recursion

---
+++ javascript/intro_javascript_exercises/phase_4_recursion.js
@@ -0,0 +1,14 @@
+// range(start, end) - receives a start and end value, returns an array from start up to end
+
+
+
+
+
+// sumRec(arr) - receives an array of numbers and recursively sums them
+
+
+
+
+
+
+// exponent(base, exp) - receives a base and exponent, returns the base raise to the power of the exponent(base ^ exp)
