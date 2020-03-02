fbac161fb4234399b1ccc561bdcbc31b3ebe9b27
Combatd <combatdstar@gmail.com>
2020-03-02 13:02:40 -0800

    Define several Student.prototype methods for name, enroll, courseLoad

--- javascript/intro_javascript_exercises/phase_6_students_and_courses.js
+++ javascript/intro_javascript_exercises/phase_6_students_and_courses.js
@@ -2,5 +2,28 @@ class Student {
     constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.courses = [];
     }
+}
+
+Student.prototype.name = function() {
+    return this.firstName + " " + this.lastName;
+}
+
+Student.prototype.enroll = function(course) {
+    if (this.courses.includes(course)) {
+        return -1;
+    }
+    
+    this.courses.push(course);
+    course.students.push(this);
+}
+
+Student.prototype.courseLoad = function() {
+    departmentHash = {};
+    this.courses.forEach(course => {
+        departmentHash.department = course.department;
+        departmentHash[department] = course.credits;
+    });
+    return departmentHash;
 }
