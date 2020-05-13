dfe514ad400e467387ab6f11954660a24b74ade0
Combatd <combatdstar@gmail.com>
2020-05-12 18:56:50 -0700

    Validate :username :password :password_digest and ensure_session_token before_validation on User Model

--- rails/rails_auth_homework/app/models/user.rb
+++ rails/rails_auth_homework/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    attr_reader :password
+
+    validates :username, :session_token, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+    validates :password_digest, presence: { message: "Password can't be blank" }
+    before_validation :ensure_session_token
 end
