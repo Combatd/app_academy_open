682b8f9feca7811dcbf2e72e4498070f73ded5e6
Combatd <combatdstar@gmail.com>
2020-05-13 10:52:53 -0700

    Define User#reset_session_token! on rails_auth_homework

--- rails/rails_auth_homework/app/models/user.rb
+++ rails/rails_auth_homework/app/models/user.rb
@@ -14,4 +14,10 @@ class User < ApplicationRecord
     def self.generate_session_token
         SecureRandom::urlsafe_base64(16)
     end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
 end
