05ef7a4e02d499bca5a50500c31229dee7ccf99b
Combatd <combatdstar@gmail.com>
2020-05-13 10:30:29 -0700

    Define User::generate_session_token on rails_auth_homework

--- rails/rails_auth_homework/app/models/user.rb
+++ rails/rails_auth_homework/app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
         user = User.find_by(username: username)
         return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
     end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
 end
