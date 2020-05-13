407ece339242c4ee8288d376760c78ec076a9558
Combatd <combatdstar@gmail.com>
2020-05-12 18:59:18 -0700

    Define User::find_by_credentials

--- rails/rails_auth_homework/app/models/user.rb
+++ rails/rails_auth_homework/app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
     validates :password, length: { minimum: 6, allow_nil: true }
     validates :password_digest, presence: { message: "Password can't be blank" }
     before_validation :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    end
 end
