d92dab49cbaaea15d149220b200a34ffa920ab24
Combatd <combatdstar@gmail.com>
2020-07-14 20:32:06 -0700

    Draw Cricle using beginPath, arc, stroke, fill

--- javascript/canvas_practice/practice.js
+++ javascript/canvas_practice/practice.js
@@ -3,8 +3,19 @@ document.addEventListener("DOMContentLoaded", function(){
     canvasElement.height = 500;
     canvasElement.width = 500;
 
+    // Draw a Rectangle
     // const ctx = canvas.getContext(contextType, contextAttributes);
     const ctx = canvasElement.getContext('2d');
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(20, 20, 500, 500);
+
+    // Draw a Circle
+    ctx.beginPath();
+    // ctx.arc(x, y, radius, startAngle, endAngle [, anticlockwise]);
+    ctx.arc(100, 100, 50, 0, 2 * Math.PI, true)
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 6;
+    ctx.stroke();
+    ctx.fillStyle = 'purple';
+    ctx.fill();
 });
