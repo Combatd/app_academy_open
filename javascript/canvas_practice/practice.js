fd202c9769ebf212f06d949ca5e66bb46acda09e
Combatd <combatdstar@gmail.com>
2020-07-14 17:44:51 -0700

    Define canvasElement and getContext 2d

--- javascript/canvas_practice/practice.js
+++ javascript/canvas_practice/practice.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const canvasElement = document.getElementById('mycanvas');
+    canvasElement.height = 500;
+    canvasElement.width = 500;
 
+    // const ctx = canvas.getContext(contextType, contextAttributes);
+    const ctx = getContext('2d');
 });
