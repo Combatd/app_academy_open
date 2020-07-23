0112632889719a34594f96ac9379d93e9d119c1c
Combatd <combatdstar@gmail.com>
2020-07-22 22:17:42 -0700

    Define hammerTIme(time)

--- javascript/callbacks-and-function-calls-homework/simple-timeout.js
+++ javascript/callbacks-and-function-calls-homework/simple-timeout.js
@@ -13,5 +13,22 @@ const simpleTimeout = (callback) => {
     setTimeout(callback, 5000);
 }
 
-simpleTimeout(function() { alert('HAMMERTIME')
-});
+// simpleTimeout(function() { alert('HAMMERTIME')
+// });
+
+/* Timeout Plus Closure
+Next, we are going to write a function hammerTime that takes in one argument - time. When the function is run, it should set a timeout on the window. Pass a callback to the timeout that creates an alert ${time} is hammertime!.
+
+Notice that the callback function closed over the time variable in the outer scope of hammerTime.
+*/
+
+// this is still an async function
+const hammerTime = (time) => {
+    console.log('entered hammerTime')
+    setTimeout(function() { 
+        alert(`${time} is hammertime!`) 
+    }, time);
+    console.log('end hammerTime');
+}
+
+hammerTime(5000);
